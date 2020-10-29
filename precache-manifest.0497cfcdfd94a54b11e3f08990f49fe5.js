ebfc374882147125e56c5aa622dd33df937b81f3
CrioDo <rahul@criodo.com>
2020-10-29 20:54:08 +0530

    Updates

---
+++ precache-manifest.0497cfcdfd94a54b11e3f08990f49fe5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85834e55d56c4688ad17c3e8047e554",
+    "url": "/portfolio-vishruthshandilya1989/index.html"
+  },
+  {
+    "revision": "061a6717d1403d534f82",
+    "url": "/portfolio-vishruthshandilya1989/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "a544a2ddf08eb4d4ef2f",
+    "url": "/portfolio-vishruthshandilya1989/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "061a6717d1403d534f82",
+    "url": "/portfolio-vishruthshandilya1989/static/js/2.e5d948b2.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/portfolio-vishruthshandilya1989/static/js/2.e5d948b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a544a2ddf08eb4d4ef2f",
+    "url": "/portfolio-vishruthshandilya1989/static/js/main.0a28d687.chunk.js"
+  },
+  {
+    "revision": "79620050064aca8cd16c",
+    "url": "/portfolio-vishruthshandilya1989/static/js/runtime-main.252716d5.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/portfolio-vishruthshandilya1989/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/portfolio-vishruthshandilya1989/static/media/crio-logo.63be71d0.svg"
+  }
+]);
